3852b83a644b3b1361ca43241543e33ffe47e36e
Aldeirfrancisco <dide123francisco@gmail.com>
2020-05-30 22:41:15 -0300

    estalamos o sucrase e instalamos o nodomon

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router} from "express";
 const routes = new  Router();
 
 routes.get('/', (resquest, response) => {
-    return  response.json({msg: "vamos la..."})
+    return  response.json({msg: "vamos de la..."})
 });
 
-export default  routes;
+export default routes;
