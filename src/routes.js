e200db9607ab056ecd7625b28d3f2aeba58df8ab
Aldeirfrancisco <dide123francisco@gmail.com>
2020-06-06 17:57:28 -0300

    configurando bloqueio de rotad sen tokem

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ import UsuarioController from './app/controller/UsuarioController';
 import PessoaController from './app/controller/PessoaController';
 import LancamentoController from './app/controller/LancamentoController';
 import AutenticadorController from './app/controller/AutenticadorController';
+import authInterceptor from './app/middlewares/authInterceptor';
 // import Usuario from './app/models/Usuario';
 
 const routes = new Router();
-
 routes.post('/awth', AutenticadorController.auth);
+routes.use(authInterceptor); // aqui para baixo precissa de token
 routes.post('/usuarios', UsuarioController.inclui);
 routes.post('/pessoas', PessoaController.inclui);
 routes.post('/Lancamento', LancamentoController.incluir);
