a668932999adb1d5bf10831bd8bcbe553e7151fa
Aldeirfrancisco <dide123francisco@gmail.com>
2020-06-11 17:51:53 -0300

    fazer busca no banco

--- web/src/App.js
+++ web/src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { ThemeProvider } from '@material-ui/styles';
-import theme from './theme';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import './assets/scss/index.scss';
+import { ToastContainer } from 'react-toastify';
 import Routes from './Routes';
+import 'react-toastify/dist/ReactToastify.css';
+import theme from './theme';
 
 const browserHistory = createBrowserHistory();
 
@@ -15,6 +17,7 @@ export default class App extends Component {
             <ThemeProvider theme={theme}>
                 <Router history={browserHistory}>
                     <Routes />
+                    <ToastContainer />
                 </Router>
             </ThemeProvider>
         );
